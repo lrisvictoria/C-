a1e6ca5545d58bb4b7de24622c70bb70aa5ebb4f
exploreranduin <a1249967801@163.com>
2023-01-31 12:04:24 +0800

    1.31 类和对象 1 完结

--- 类和对象(一)/test.cpp
+++ 类和对象(一)/test.cpp
@@ -124,27 +124,112 @@ using namespace std;
 //	return 0;
 //}
 
-class ch // 大小为 1
-{
-	char c;
-};
+//class ch // 大小为 1
+//{
+//	char c;
+//};
+//
+//class null // 空
+//{
+//};
+//
+//class nom // 只有成员方法
+//{
+//	void foo() {}
+//};
+//
+//int main()
+//{
+//	ch a;
+//	null b;
+//	nom c;
+//
+//	cout << "a: " << sizeof(a) << ' ' << "b: " << sizeof(b) << ' ' << "c: " << sizeof(c);
+//
+//	return 0;
+//}
 
-class null // 空
-{
-};
+//class Date
+//{
+//public:
+//	void Init(int year, int month, int day)
+//	{
+//		Date::year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//private:
+//	int year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date s;
+//	s.Init(2023, 1, 31);
+//
+//	return 0;
+//}
 
-class nom // 只有成员方法
-{
-	void foo() {}
-};
+//void Init(Date* this, int year, int month, int day)
+//{
+//	this->_year = year;
+//	this->_month = month;
+//	this->_day = day;
+//}
 
+//class Date
+//{
+//public:
+//	void Init(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << this->_year << "-" << this->_month << "-" << this->_day << endl;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//	d1.Init(2023, 1, 31);
+//	d1.Print();
+//
+//	Date d2;
+//	d2.Init(2023, 2, 1);
+//	d2.Print();
+//
+//	return 0;
+//}
+
+
+
+// 1.下面程序编译运行结果是？ A、编译报错 B、运行崩溃 C、正常运行
+class A
+{
+public:
+	void PrintA()
+	{
+		cout << _a << endl;
+	}
+private:
+	int _a;
+};
 int main()
 {
-	ch a;
-	null b;
-	nom c;
-
-	cout << "a: " << sizeof(a) << ' ' << "b: " << sizeof(b) << ' ' << "c: " << sizeof(c);
-
+	A* p = nullptr;
+	p->PrintA();
 	return 0;
 }
