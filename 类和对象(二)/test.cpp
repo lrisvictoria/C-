aa4d18a3d1d234a0e86fa2ebb75481b4967e0128
exploreranduin <a1249967801@163.com>
2023-02-01 13:44:48 +0800

    2.1 update

--- 类和对象(二)/test.cpp
+++ 类和对象(二)/test.cpp
@@ -76,17 +76,41 @@ using namespace std;
 //	return 0;
 //}
 
-class A
-{
-public:
-	A()
-	{
-		cout << "A()" << endl;
-		_a = 0;
-	}
-private:
-	int _a;
-};
+//class A
+//{
+//public:
+//	A()
+//	{
+//		cout << "A()" << endl;
+//		_a = 0;
+//	}
+//private:
+//	int _a;
+//};
+//
+//class Date
+//{
+//public:
+//	Date(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//	A _aa;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//
+//	return 0;
+//}
 
 class Date
 {
@@ -97,12 +121,16 @@ public:
 		_month = month;
 		_day = day;
 	}
+	~Date()
+	{
+		// Date 类没有资源需要清理，所以 Date 类不识闲析构函数都可以
+		cout << "~Date()" << endl;
+	}
 
 private:
 	int _year;
 	int _month;
 	int _day;
-	A _aa;
 };
 
 int main()
