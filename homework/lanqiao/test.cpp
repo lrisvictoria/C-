d6f9261a50a5ec7a2766c563eea541ef9819c1b3
exploreranduin <a1249967801@163.com>
2023-03-05 19:59:55 +0800

    练习

---
+++ homework/lanqiao/test.cpp
@@ -0,0 +1,41 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(0), cout.tie(0);
+//
+//	int day1, day2;
+//	cin >> day1 >> day2;
+//
+//	int sum = day1 + day2;
+//	cout << (sum % 7) << endl;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a[] = { 99, 22, 51, 63, 72, 61, 20, 88, 40, 21, 63, 30, 11, 18, 99, 12, 93, 16, 7, 53, 64, 9, 28, 84, 34, 96, 52, 82, 51, 77 };
+//	long long res = 0;
+//	int cnt = 0;
+//	for (int i = 0; i < 30; i++)
+//	{
+//		for (int j = 0; j < 30; j++)
+//		{
+//			if (a[i] * a[j] > 2022)
+//			{
+//				cnt++;
+//			}
+//		}
+//	}
+//
+//	cout << cnt / 2 << endl;
+//	return 0;
+//}
