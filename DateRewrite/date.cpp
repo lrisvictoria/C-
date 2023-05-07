862bf41bd8e8b651b3cc110e53342f5ed802489a
exploreranduin <a1249967801@163.com>
2023-05-07 19:27:43 +0800

    重写日期类

--- DateRewrite/date.cpp
+++ DateRewrite/date.cpp
@@ -72,3 +72,138 @@ Date& Date::operator=(const Date& d) // 引用返回减少拷贝构造
 
 	return *this;
 }
+
+bool Date::operator>(const Date& d)
+{
+	if (_year > d._year)
+	{
+		return true;
+	}
+	else if (_year == d._year && _month > d._month)
+	{
+		return true;
+	}
+	else if (_year == d._year && _month == d._month && _day > d._day)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool Date::operator<=(const Date& d)
+{
+	return !(*this > d);
+}
+
+bool Date::operator==(const Date& d)
+{
+	return _year == d._year
+		&& _month == d._month
+		&& _day == d._day;
+}
+
+bool Date::operator>=(const Date& d)
+{
+	return *this > d || *this == d;
+}
+
+bool Date::operator<(const Date& d)
+{
+	return !(*this >= d);
+}
+
+bool Date::operator>=(const Date& d)
+{
+	return !(*this < d);
+}
+
+bool Date::operator!=(const Date& d)
+{
+	return !(*this == d);
+}
+
+Date& Date::operator+=(int day)
+{
+	if (day < 0)
+	{
+		return *this -= day;
+	}
+	_day += day;
+	while (_day > GetMonthDay(_year, _month))
+	{
+		_day -= GetMonthDay(_year, _month);
+		_month++;
+		if (_month == 13)
+		{
+			++_year;
+			_month = 1;
+		}
+	}
+	return *this;
+}
+
+Date Date::operator+(int day)
+{
+	// 拷贝构造
+	Date tmp(*this);
+	tmp += day;
+	return tmp;
+}
+
+Date& Date::operator-=(int day)
+{
+	if (day > 0)
+	{
+		return *this += day;
+	}
+	_day -= day;
+
+	while (_day <= 0)
+	{
+		--_month;
+		if (_month == 0)
+		{
+			--_year;
+			_month = 12;
+		}
+		_day += GetMonthDay(_year, _month);
+	}
+	return *this;
+}
+
+Date Date::operator-(int day)
+{
+	Date tmp(*this);
+
+	tmp -= day;
+	return tmp;
+}
+
+Date& Date::operator++()
+{
+	*this += 1;
+	return *this;
+}
+
+Date Date::operator++(int)
+{
+	Date tmp(*this);
+	*this += 1;
+	return tmp;
+}
+
+Date& Date::operator--()
+{
+	*this -= 1;
+	return *this;
+}
+
+Date Date::operator--(int)
+{
+	Date tmp(*this);
+	*this -= 1;
+	return tmp;
+}
--- DateRewrite/date.h
+++ DateRewrite/date.h
@@ -13,7 +13,23 @@ public:
 	void Print();
 
 	bool operator<(const Date& d);
-	Date& operator=(const Date& d);
+	Date& operator=(const Date& d); // 赋值重载
+	bool operator>(const Date& d);
+	bool operator<(const Date& d);
+	bool operator>=(const Date& d);
+	bool operator<= (const Date& d);
+	bool operator==(const Date& d);
+	bool operator!=(const Date& d);
+	// += + -= -
+	Date& operator+=(int day);
+	Date operator+(int day);
+	Date& operator-=(int day);
+	Date operator-(int day);
+	Date& operator++();
+	Date operator++(int);
+	Date& operator--();
+	Date operator--(int);
+	
 private:
 	int _year;
 	int _month;
@@ -21,95 +37,31 @@ private:
 };
 
 /*
-class Date
-
-{
-
 public:
   // 日期+=天数
 
 Date& operator+=(int day);
-
-
-
   // 日期+天数
 
 Date operator+(int day);
-
-
-
   // 日期-天数
 
 Date operator-(int day);
-
-
-
    // 日期-=天数
 
 Date& operator-=(int day);
-
-
-
   // 前置++
 
 Date& operator++();
-
-
-
   // 后置++
 
 Date operator++(int);
-
-
-
   // 后置--
 
 Date operator--(int);
-
-
-
   // 前置--
 
 Date& operator--();
-
-
-
-  // >运算符重载
-
-bool operator>(const Date& d);
-
-
-
-  // ==运算符重载
-
-bool operator==(const Date& d);
-
-
-
-  // >=运算符重载
-
-bool operator >= (const Date& d);
-
-
-
-  // <运算符重载
-
-bool operator < (const Date& d);
-
-
-
-   // <=运算符重载
-
-bool operator <= (const Date& d);
-
-
-
-  // !=运算符重载
-
-bool operator != (const Date& d);
-
-
-
   // 日期-日期 返回天数
 
 int operator-(const Date& d);
