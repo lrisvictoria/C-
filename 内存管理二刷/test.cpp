7c0cf0eb6989cb89efe1960d6b40ff00c1d6e8a1
exploreranduin <a1249967801@163.com>
2023-05-17 18:58:22 +0800

    5.17 内存管理刷完

--- 内存管理二刷/test.cpp
+++ 内存管理二刷/test.cpp
@@ -1,10 +1,54 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//class A
+//{
+//public:
+//	A(int a, int b)
+//		:_a(a) 
+//		,_b(b)
+//	{}
+//private:
+//	int _a;
+//	int _b;
+//};
+//
+//int main()
+//{
+//	A* a1 = new A(1, 2);
+//	A* a2 = new A[4]{ {1, 2}, {1, 2}, {1, 2}, {1, 2} };
+//
+//
+//
+//	return 0;
+//}
+
 #include <iostream>
 
 using namespace std;
 
+class A
+{
+public:
+	A(int a = 0)
+		:_a(a)
+	{
+		cout << "A()" << endl;
+	}
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+private:
+	int _a;
+};
+
 int main()
 {
-	int n, m;
+	A* p = new A(4);
+
+	delete p;
 
 	return 0;
 }
