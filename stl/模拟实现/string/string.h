24f05910e2ff75ced684eb7f2056f31ef117bf6a
exploreranduin <a1249967801@163.com>
2023-07-07 22:05:03 +0800

    string 类完成

--- stl/模拟实现/string/string.h
+++ stl/模拟实现/string/string.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <iostream>
-#include <cassert>
+#include <assert.h>
 
-using std::cout;
-using std::cin;
-using std::endl;
 
 namespace anduin
 {
@@ -42,12 +38,14 @@ namespace anduin
 			_str[0] = '\0';
 		}*/
 
+		// strcpy 可以不改，因为初始化时，遇到 \0 就是停止
 		string(const char* str = "") // 缺省参数
 		{
 			_size = strlen(str);
 			_capacity = _size;
 			_str = new char[_capacity + 1]; // 开辟空间
-			strcpy(_str, str);
+			// strcpy(_str, str);
+			memcpy(_str, str, _size + 1);
 		}
 		~string()
 		{
@@ -78,13 +76,400 @@ namespace anduin
 			return _str[pos];
 		}
 
+		void resize(size_t n, char ch = '\0')
+		{
+			// 删
+			if (n < _size)
+			{
+				_size = n;
+				_str[_size] = '\0';
+			}
+			else // 增
+			{
+				reserve(n); // reserve 会自己检查是否需要扩容
+				for (size_t i = _size; i < n; i++) // 从之前的 _size 开始，移一直到 n
+				{
+					_str[i] = ch;
+				}
+				_size = n;
+				_str[_size] = '\0';
+			}
+		}
+
+
+
+		void reserve(size_t n) // n 为容量大小
+		{
+			if (n > _capacity)
+			{
+				char* tmp = new char[n + 1]; // 1 给 \0 
+				// strcpy(tmp, _str);
+				memcpy(tmp, _str, _size + 1); // 拷贝 \0 
+				delete[] _str;
+				_str = tmp;
+				_capacity = n;
+				cout << "reserve -> " << _capacity << endl;
+			}
+		}
+
+		void push_back(char ch)
+		{
+			if (_size + 1 > _capacity)
+			{
+				int newcapacity = _capacity == 0 ? 4 : (2 * _capacity);
+				reserve(newcapacity);
+			}
+			_str[_size++] = ch;
+			_str[_size] = '\0';
+		}
+
+		void append(const char* str)
+		{
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				int newcapacity =  _size + len; // 至少扩到 _size  + len
+				reserve(newcapacity);
+			}
+			
+			// strcpy(_str + _size, str); // 会把 \0 拷贝过去
+			memcpy(_str + _size, str, len + 1); 
+			_size += len;
+		}
 		
+		string& operator+=(char ch)
+		{
+			push_back(ch);
+			return *this;
+		}
+
+		string& operator+=(const char* str)
+		{
+			append(str);
+			return *this;
+		}
+
+		void insert(size_t pos, size_t n, char ch) // pos 位置 n 个字符
+		{
+			assert(pos <= _size); // 断言
+			if (_size + n > _capacity)
+			{
+				int newcapacity = _size + n; // 至少扩容到 _size + n 
+				reserve(newcapacity);
+			}
+			// 挪动数据
+			// plan 1
+			size_t end = _size;
+			while (end >= pos && end != npos) // 一直挪到 pos，判断是否等于 npos
+			{
+				_str[end + n] = _str[end];
+				--end;
+			}
+			for (size_t i = pos; i < n + pos; i++)
+			{
+				_str[i] = ch;
+			}
+			
+			// plan2: 将 end 位置改变
+			/*size_t end = _size + n;
+			while (end > pos)
+			{
+				_str[end] = _str[end - n];
+				--end;
+			}
+			for (size_t i = pos; i < n + pos; i++)
+			{
+				_str[i] = ch;
+			}
+			*/
+
+			_size += n;
+		}
+
+		void insert(size_t pos, const char* str)
+		{
+			assert(pos <= _size);
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				int newcapacity = _size + len;
+				reserve(newcapacity);
+			}
+
+			size_t end = _size;
+			while (end >= pos && end != npos)
+			{
+				_str[end + len] = _str[end];
+				--end;
+			}
+
+			for (size_t i = 0; i < len; i++)
+			{
+				_str[pos + i] = str[i];
+			}
+
+			_size += len;
+		}
+
+		void erase(size_t pos, size_t len = npos)
+		{
+			assert(pos < _size);
+
+			if (len == npos || len + pos >= _size)
+			{
+				_str[pos] = '\0';
+				_size = pos;
+			}
+			else
+			{
+				size_t  end = pos + len;
+				while (end <= _size)
+				{
+					_str[pos++] = _str[end++];
+				}
+				_size -= len;
+			}
+		}
+		
+
+		size_t find(char ch, size_t pos = 0)
+		{
+			assert(pos < _size);
+			for (size_t i = pos; i < _size; i++)
+			{
+				if (_str[i] == ch)
+				{
+					return i;
+				}
+			} 
+			return npos; // 没找到
+		}
+
+
+		size_t find(const char* str, size_t pos = 0)
+		{
+			assert(pos < _size);
+			const char* ptr = strstr(_str + pos, str); // 在 _str 中查找 str
+			if (ptr)
+			{
+				return ptr - _str; // 指针 - 指针，无论 pos 在哪里，都是显示的整体位置
+			}
+			else
+			{
+				return npos;
+			}
+		}
+
+		
+		string substr(size_t pos = 0, size_t len = npos)
+		{
+			assert(pos < _size);
+
+			size_t n = len; // 取 len 个字符
+			if (len == npos || len + pos >= _size) // 直接截完
+			{
+				n = _size - pos; // 实际长度
+			}
+			
+			string tmp;
+			tmp.reserve(n); // 开好空间
+			for (size_t i = pos; i < n + pos; i++) 
+			{
+				tmp += _str[i];
+			}
+			return tmp; // 返回需要拷贝构造
+		}
+
+		// 拷贝构造，深拷贝
+		string(const string& s)
+		{
+			_str = new char[s._capacity + 1];
+			// strcpy(_str, s._str);
+			memcpy(_str, s._str, s._size + 1); // 需要完整拷贝
+			_size = s._size;
+			_capacity = s._capacity;
+		}
+
+		void clear()
+		{
+			_str[0] = '\0';
+			_size = 0;
+		}
+		
+		// 自己写
+		//bool operator<(const string& s)
+		//{
+		//	size_t i1 = 0, i2 = 0;
+		//	while (i1 < _size && i2 < s._size)
+		//	{
+		//		if (_str[i1] < _str[i2])
+		//		{
+		//			return true;
+		//		}
+		//		else if (_str[i1] > s._str[i2])
+		//		{
+		//			return false;
+		//		}
+		//		else
+		//		{
+		//			i1++, i2++;
+		//		}
+		//	}
+
+		//	// hello hello
+		//	// helloxx hello
+		//	// hello helloxx
+		//	return i1 == _size && i2 != s._size;
+		//	return _size < s._size;
+		//}
+		
+		// 复用
+		bool operator<(const string& s) const
+		{
+			int ret = memcmp(_str, s._str, _size < s._size ? _size : s._size);
+			return ret == 0 ? _size < s._size : ret < 0;
+		}
+
+		bool operator==(const string& s) const
+		{
+			return _size == s._size && memcmp(_str, s._str, _size) == 0;
+		}
+
+		bool operator<=(const string& s) const
+		{
+			return *this < s || *this == s;
+		}
+
+		bool operator>(const string& s) const
+		{
+			return !(*this <= s);
+		}
+
+		bool operator>=(const string& s) const
+		{
+			return !(*this < s);
+		}
+
+		bool operator!=(const string& s) const
+		{
+			return !(*this == s);
+		}
+
+		// 赋值重载 传统写法
+		//string& operator=(const string& s)
+		//{
+		//	// 防止自己给自己赋值
+		//	if (this != &s)
+		//	{
+		//		char* tmp = new char[s._capacity + 1];
+		//		memcpy(tmp, s._str, s._capacity + 1);
+		//		delete[] _str;
+
+		//		_str = tmp;
+		//		_size = s._size;
+		//		_capacity = s._capacity;
+		//	}
+
+		//	return *this;
+		//}
+
+		// 现代写法 part 1
+		//string& operator=(const string& s)
+		//{
+		//	if (this != &s)
+		//	{
+		//		string tmp(s); // 拷贝构造 s
+		//		std::swap(_str, tmp._str); // 改变指向
+		//		std::swap(_size, tmp._size);
+		//		std::swap(_capacity, tmp._capacity);
+
+		//		// err
+		//		// std::swap(*this, tmp); // 无限递归调用赋值
+		//	}
+		//	return *this;
+		//}
+
+		void swap(string& s)
+		{
+			std::swap(_str, s._str);
+			std::swap(_size, s._size);
+			std::swap(_capacity, s._capacity);
+		}
+
+		//string& operator=(const string& s)
+		//{
+		//	if (this != &s)
+		//	{
+		//		string tmp(s);
+		//		swap(tmp); // 调换 _str 和 tmp
+		//	}
+		//	return *this;
+		//}
+
+		// 现代写法 part 2
+		string& operator=(string tmp) // 拷贝构造 tmp ，交换 tmp h和 _str
+		{
+			swap(tmp);
+			return *this;
+		}
 
 	private:
 		char* _str;
 		size_t _capacity;
 		size_t _size;
-	};
 
-	
-}
+		static size_t npos;
+		// const static size_t npos = -1; // right，但是不建议这么写
+		// const static double x = 1.1; // error
+		// const static long y = 1; // right
+	};
+	size_t string::npos = -1;
+
+	ostream& operator<<(ostream& out, const string& s)
+	{
+		/*for (size_t i = 0; i < s.size(); i++)
+		{
+			out << s[i];
+		}*/
+
+		for (auto ch : s)
+		{
+			out << ch;
+		}
+
+		return out;
+	}
+
+	istream& operator>>(istream& in, string& s)
+	{
+		s.clear();
+
+		char ch = in.get();
+		while (ch == ' ' || ch == '\n')
+		{
+			ch = in.get();
+		}
+
+		char buf[128];
+		int i = 0;
+		while (ch != ' ' && ch != '\n')
+		{
+			buf[i++] = ch;
+			if (i == 127)
+			{
+				buf[i] = '\0';
+				s += buf;
+				i = 0;
+			}
+			ch = in.get();
+		}
+		if (i != 0)
+		{
+			buf[i] = '\0';
+			s += buf;
+		}
+
+		return in;
+	}
+};
+
--- stl/模拟实现/string/test.cpp
+++ stl/模拟实现/string/test.cpp
@@ -1,13 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
 
-#include "string.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-using namespace anduin;
+
+#include "string.h"
 
 void test_string1()
 {
-	string s1("hello world");
-	string s2; // 有默认构造（带缺省）
+	anduin::string s1("hello world");
+	anduin::string s2; // 有默认构造（带缺省）
 
 	cout << s1.c_str() << endl;
 	cout << s2.c_str() << endl;
@@ -21,8 +24,8 @@ void test_string1()
 
 void test_string2()
 {
-	string s1("hello world");
-	string::iterator it = s1.begin();
+	anduin::string s1("hello world");
+	anduin::string::iterator it = s1.begin();
 	while (it != s1.end())
 	{
 		*it += 1;
@@ -37,14 +40,135 @@ void test_string2()
 		cout << e << ' ';
 	}
 
-	const string s2("hello anduin");
-	string::const_iterator it2= s2.begin();
+	const anduin::string s2("hello anduin");
+	anduin::string::const_iterator it2= s2.begin();
 	s2[0];
+}
+
+void test_string3()
+{
+	anduin::string s1("hello world");
+	s1.push_back('0');
+
+	cout << s1.c_str() << endl;
+
+	s1.append("hello");
+	cout << s1.c_str() << endl;
+
+	s1 += '#';
+	cout << s1.c_str() << endl;
+	s1 += "hello";
+	cout << s1.c_str() << endl;
+
+}
+
+void test_string4()
+{
+	anduin::string s1("hello world");
+	/*s1.insert(6, 3, '0');
+	cout << s1.c_str() << endl;
+
+	s1.insert(0, 3, '0');
+	cout << s1.c_str() << endl;*/
+
+	s1.insert(5, "hello hello");
+	cout << s1.c_str() << endl;
+}
+
+void test_string5()
+{
+	anduin::string s1("hello world");
+	s1.erase(6, 3);
+	cout << s1.c_str() << endl;
+
+	s1.erase(2);
+	cout << s1.c_str() << endl;
+
+}
+
+void test_string6()
+{
+	anduin::string s1("hello world");
+	cout << s1.find('o', 6) << endl;
+	cout << s1.find('o') << endl;
+
+	cout << (int)s1.find("hel", 6) << endl;
+	cout << s1.find("hel") << endl;
+
+	cout << (s1.substr(1, 3)).c_str() << endl;
+	cout << (s1.substr(1)).c_str() << endl;
+}
+
+void test_string7()
+{
+	anduin::string s1("hello world");
+	/*s1.resize(8);
+	s1.resize(13, 'x');
+	s1.resize(20, 'y');*/
+
+	s1 += '\0';
+	s1 += "hello";
+
+	cout << s1.c_str() << endl; // 打印 c 字符串，遇到 \0 就停止
+	cout << s1 << endl; // 都会打印
+
+	anduin::string s2(s1);
+	cout << s2 << endl;
+}
+
+void test_string8()
+{
+	anduin::string s1;
+	cin >> s1;
+	cout << s1 << endl;
+
+	cin >> s1;
+	cout << s1;
+}
+
+void test_string9()
+{
+	anduin::string s1("hello");
+	anduin::string s2("xxxxxxxxxxxxxxxxx");
+
+	s1 = s2;
+	cout << s1 << endl;
+	cout << s2 << endl;
 }
 
 int main()
 {
-	// test_string1();
-	test_string2();
+	// test_string3();
+	// test_string2();
+
+	// test_string6();
+	
+
+	/*char str[100];
+	scanf("%s", str);
+	printf("%s", str);*/
+	// test_string8();
+
+	anduin::string s1("hello");
+	anduin::string s2("hello");
+
+	cout << (s1 < s2) << endl;
+	cout << (s1 == s2) << endl;
+
+	anduin::string s3("hello");
+	anduin::string s4("helloxx");
+	cout << (s3 < s4) << endl;
+	cout << (s3 > s4) << endl;
+
+	
+	anduin::string s5("helloxxxx");
+	anduin::string s6("hello");
+	cout << (s5 < s6) << endl;
+	cout << (s5 > s6) << endl;
+
+	test_string9();
+
+
 	return 0;
 }
+
