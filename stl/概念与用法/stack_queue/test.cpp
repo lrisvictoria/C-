16b6377f967e5041377190a0eb0e0c9003ff98ff
exploreranduin <a1249967801@163.com>
2023-07-19 20:47:02 +0800

    测试 stack_queue_deque

--- stl/概念与用法/stack_queue/test.cpp
+++ stl/概念与用法/stack_queue/test.cpp
@@ -55,6 +55,56 @@ void test()
 	cout << endl;
 }
 
+void test_op()
+{
+	srand(time(0));
+	const int N = 1000000;
+	vector<int> v1;
+	vector<int> v2;
+	v1.reserve(N);
+	v2.reserve(N);
+
+	deque<int> dq1;
+	deque<int> dq2;
+
+
+	for (int i = 0; i < N; ++i)
+	{
+		auto e = rand();
+		dq1.push_back(e);
+		dq2.push_back(e);
+		// v2.push_back(e);
+	}
+
+	int begin1 = clock();
+	// 先拷贝到vector
+	for (auto e : dq1)
+	{
+		v1.push_back(e);
+	}
+
+	// 排序
+	sort(v1.begin(), v1.end());
+
+	// 拷贝回去
+	size_t i = 0;
+	for (auto& e : dq1)
+	{
+		e = v1[i++];
+	}
+
+	int end1 = clock();
+
+	int begin2 = clock();
+	// sort(v2.begin(), v2.end()); // deque copy 直接和 vector sort 比较，拷贝的代价很低，所以两者比较相差不大
+	sort(dq2.begin(), dq2.end());
+
+	int end2 = clock();
+	// deque拷贝到 vector sort 和 deque 直接 sort 比较，deque 慢
+	printf("deque copy vector sort:%d\n", end1 - begin1);
+	printf("deque sort:%d\n", end2 - begin2);
+}
+
 int main()
 {
 	/* Container adaptor
@@ -70,7 +120,7 @@ int main()
 	// 没有迭代器，不支持随便遍历
 
 	//test_stack_queue();
-	test();
+	test_op();
 
 	return 0;
 }
