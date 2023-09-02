c342f55a9e1fea95e2ae3b8956180ba69536f868
exploreranduin <a1249967801@163.com>
2023-09-02 12:57:08 +0800

    update

---
+++ 进阶/异常/test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+double Division(int a, int b)
+{
+	// 当b == 0时抛出异常
+	if (b == 0)
+		throw "Division by zero condition!";
+	else
+		return ((double)a / (double)b);
+}
+void Func()
+{
+	int len, time;
+	cin >> len >> time;
+	cout << Division(len, time) << endl;
+}
+int main()
+{
+	try {
+		Func();
+	}
+	catch (const char* errmsg) {
+		cout << errmsg << endl;
+	}
+
+	return 0;
+}
